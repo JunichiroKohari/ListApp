1d4aecdb7a1dd7c3dad5265d754ec00eb920902f
JunichiroKohari <junichiro@kohari.jp>
2020-12-18 05:20:32 +0900

    Initial Commit

---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by Junichiro Kohari on 2020/12/18.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
