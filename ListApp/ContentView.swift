d61e86b1665e4d9bdac33dfbebd576305eab38d0
JunichiroKohari <junichiro@kohari.jp>
2021-01-24 21:10:45 +0900

    Navigation View をHello World にドラッグ＆ドロップ

--- ListApp/ContentView.swift
+++ ListApp/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!!!")
-            .padding()
+        NavigationView {
+            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
+        }
     }
 }
 
